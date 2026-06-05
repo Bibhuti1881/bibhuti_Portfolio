3382c22bf557251814592b4b04bc4a7d79263348
Bibhuti Adhikari <97746528+Bibhuti1881@users.noreply.github.com>
2026-06-05 21:13:43 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing Animation
 var typed = new Typed(".typing", {
-    strings: ["", "Web Designer", "Web Developer", "Graphic Designer"],
+    strings: ["", "Python Developer", "Web Developer", "Web Scraper"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
